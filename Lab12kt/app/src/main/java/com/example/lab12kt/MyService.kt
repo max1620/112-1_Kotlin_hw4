b1d3cb3ea1075cba7dfbcf97fdbda7db2db528fe
max1620 <x35713712@gmail.com>
2023-12-11 16:58:44 +0800

    Second commit

--- Lab12kt/app/src/main/java/com/example/lab12kt/MyService.kt
+++ Lab12kt/app/src/main/java/com/example/lab12kt/MyService.kt
@@ -3,11 +3,16 @@ package com.example.lab12kt
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import kotlinx.coroutines.*
 
 class MyService : Service() {
+
+    val serviceJob = Job()
+    val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
     override fun onCreate() {
         super.onCreate()
-        Thread {
+
+        /*Thread {
             try {
                 Thread.sleep(3000)
                 val intent = Intent(this, SecActivity::class.java)  //intent從service啟動SecActivity
@@ -16,10 +21,26 @@ class MyService : Service() {
             } catch (e: InterruptedException) {
                 e.printStackTrace()
             }
+        }*/
+
+        serviceScope.launch {
+            delay(3000)
+            startActivity()
         }.start()
     }
     override fun onStartCommand(intent: Intent, flags: Int, startid: Int): Int {
         return START_NOT_STICKY
     }
     override fun onBind(intent: Intent): IBinder? = null
+
+    private fun startActivity() {
+        val intent = Intent(this, SecActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceJob.cancel() // 在服務銷毀時取消所有的協程
+    }
 }
